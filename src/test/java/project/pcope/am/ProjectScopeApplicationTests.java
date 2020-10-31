2fea2d183c35051df33a8286f0de8a628df2c060
RafoMeliqyan <rafomelikyan10@gmail.com>
2020-10-31 09:53:53 +0400

    Initial commit

---
+++ src/test/java/project/pcope/am/ProjectScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package project.pcope.am;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectScopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
